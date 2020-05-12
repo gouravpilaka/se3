8f4a228489924fba48dbc2ac860ff05d4ef73769
Gourav <60485831+gouravpilaka@users.noreply.github.com>
2020-05-12 02:52:41 -0400

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,12 +1,12 @@
 <?php
 
 $servername = "tcp:localhost01.database.windows.net,1433";
-$username = "pilakag";
+$username = "pilakag1";
 $password = "Pilakag_1";
 $db = "pilakag1_db";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password,$db);
+$conn = new mysqli_connect($servername, $username, $password,$db);
 // Check connection
 if (!$conn) {
    die("Connection failed: " . mysqli_connect_error());
