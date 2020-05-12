fb5d645aa95b5bf30cc4b6011dcc80ad035c31f6
Gourav <60485831+gouravpilaka@users.noreply.github.com>
2020-05-12 02:39:03 -0400

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,18 +1,15 @@
 <?php
 
-  $servername = "tcp:localhost01.database.windows.net,1433";
-	$username = "pilakag1";
-	$password = "Pilakag_1";
-	$db = "pilakag1_db";
+$servername = "tcp:localhost01.database.windows.net,1433";
+$username = "pilakag1";
+$password = "Pilakag_1";
+$db = "pilakag1_db";
 
-try {
-   $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password, $db);
-   // set the PDO error mode to exception
-   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   echo "Connected successfully";
-   }
-catch(PDOException $e)
-   {
-   echo "Connection failed: " . $e->getMessage();
-   }
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$db);
+// Check connection
+if (!$conn) {
+   die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
 ?>
