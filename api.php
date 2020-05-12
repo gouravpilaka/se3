4d2ceb205ef73125f14c3c0c6c5aa7ac42546da5
Gourav <60485831+gouravpilaka@users.noreply.github.com>
2020-05-11 22:01:05 -0400

    done

---
+++ api.php
@@ -0,0 +1,46 @@
+<?php
+header("Content-Type:application");
+require "data.php";
+echo "<br>";
+echo '<html> Products: chilli, rub ,clove, cajun, garlic, turmeric<form>Enter Product Name: <input type="text" name="name"> <input type="submit"> </form></html>';
+
+if(!empty($_GET['name']))
+{
+	$name=$_GET['name'];
+	$price = get_price($name);
+	$avail = get_avail($name);
+	$delv = get_delv($name);
+	$dema = get_dema($name);
+
+	
+	if(empty($price))
+	{
+		response(200,"Comodity Not Found",NULL,NULL,NULL,NULL);
+	}
+	else
+	{
+		response(200,"Comodity Found",$price,$avail,$delv,$dema);
+		echo '<br>';
+			}	
+}
+else
+{
+	response(400,"Invalid Request",NULL,NULL,NULL,NULL);
+}
+
+function response($status,$status_message,$data1,$data2,$data3,$data4)
+{
+	
+	
+	$response['status']=$status;
+	$response['status_message']=$status_message;
+	$response['Price']=$data1;
+	$response['Availability']=$data2;
+	$response['Expected Delivery']=$data3;
+	$response['Demand']=$data4;
+
+	
+	$json_response = json_encode($response);
+	echo $json_response;
+}
+?>
---
+++ data.php
@@ -0,0 +1,148 @@
+<?php
+
+function get_price($name)
+{
+        
+	$servername = "localhost01";
+	$username = "pilakag1";
+	$password = "Pilakag_1";
+	$dbname = "pilakag1_db";
+
+       
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        
+        if ($conn->connect_error) 
+	{
+            die("Connection failed: " .$conn->connect_error);
+       	}
+
+       $sql = "SELECT price FROM product WHERE product = '$name'";
+
+       $result = $conn->query($sql);
+	
+
+         if ($result->num_rows > 0) {
+             
+             while($row = $result->fetch_assoc()) {
+                      $price = $row["price"];
+      }
+    } 
+else {
+                     $price = null;
+        }
+
+    $conn->close();
+
+    return $price;
+}
+
+function get_avail($name)
+{
+        
+	$servername = "localhost01";
+	$username = "pilakag1";
+	$password = "Pilakag_1";
+	$dbname = "pilakag1_db";
+
+       
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        
+        if ($conn->connect_error) 
+	{
+            die("Connection failed: " .$conn->connect_error);
+       	}
+
+     	$sql1 = "SELECT avail FROM product WHERE product = '$name'";
+
+       	$result1 = $conn->query($sql1);
+
+         if ($result1->num_rows > 0) {
+             
+             while($row = $result1->fetch_assoc()) {
+                      $avail = $row["avail"];
+      }
+    } 
+else {
+                     $avail = null;
+        }
+
+    $conn->close();
+
+    return $avail;
+}
+
+function get_delv($name)
+{
+        
+	$servername = "localhost01";
+	$username = "pilakag1";
+	$password = "Pilakag_1";
+	$dbname = "pilakag1_db";
+
+       
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        
+        if ($conn->connect_error) 
+	{
+            die("Connection failed: " .$conn->connect_error);
+       	}
+
+     	$sql1 = "SELECT delv FROM product WHERE product = '$name'";
+
+       	$result3 = $conn->query($sql1);
+
+         if ($result3->num_rows > 0) {
+             
+             while($row = $result3->fetch_assoc()) {
+                      $delv = $row["delv"];
+      }
+    } 
+else {
+                     $delv = null;
+        }
+
+    $conn->close();
+
+    return $delv;
+}
+
+function get_dema($name)
+{
+        
+	$servername = "localhost01";
+	$username = "pilakag1";
+	$password = "Pilakag_1";
+	$dbname = "pilakag1_db";
+
+       
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        
+        if ($conn->connect_error) 
+	{
+            die("Connection failed: " .$conn->connect_error);
+       	}
+
+     	$sql1 = "SELECT demand FROM product WHERE product = '$name'";
+
+       	$result4 = $conn->query($sql1);
+
+         if ($result4->num_rows > 0) {
+             
+             while($row = $result4->fetch_assoc()) {
+                      $dema = $row["demand"];
+      }
+    } 
+else {
+                     $dema = null;
+        }
+
+    $conn->close();
+
+    return $dema;
+}
+
+
+
+
+?>
+
