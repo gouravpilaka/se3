9695a12b7d15cbce699a791808d327f034c2cf5f
Gourav <60485831+gouravpilaka@users.noreply.github.com>
2020-05-15 13:45:16 -0400

    Update db_con.php

--- db_con.php
+++ db_con.php
@@ -1,5 +1,5 @@
 <?php
-$host = 'localhost01.database.windows.net';
+$host = 'tcp:localhost01.database.windows.net';
 $username = 'pilakag1@localhost01';
 $password = 'Pilakag_1';
 $db_name = 'pilakag1_db';
