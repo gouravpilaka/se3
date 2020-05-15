efd77baf8e9ed59d09966b66d9302960f237190b
Gourav <60485831+gouravpilaka@users.noreply.github.com>
2020-05-15 13:43:42 -0400

    Update db_con.php

--- db_con.php
+++ db_con.php
@@ -1,12 +1,12 @@
 <?php
 $host = 'localhost01.database.windows.net';
-$username = 'pilakag1';
+$username = 'pilakag1@localhost01';
 $password = 'Pilakag_1';
 $db_name = 'pilakag1_db';
 
 //Establishes the connection
 $conn = mysqli_init();
-mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 1433);
 if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
