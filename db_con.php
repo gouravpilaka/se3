4e8fa4776e6305ab8e920150aa651752322d3429
Gourav <60485831+gouravpilaka@users.noreply.github.com>
2020-05-15 13:37:53 -0400

    Update db_con.php

--- db_con.php
+++ db_con.php
@@ -1,15 +1,29 @@
 <?php
+$host = 'localhost01.database.windows.net';
+$username = 'pilakag1@localhost01';
+$password = 'Pilakag_1';
+$db_name = 'pilakag1_db';
 
-$servername = "localhost01.database.windows.net,143";
-$username = "pilakag1";
-$password = "Pilakag_1";
-$db = "pilakag1_db";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password,$db);
-// Check connection
-if (!$conn) {
-   die("Connection faild: " . mysqli_connect_error());
+//Establishes the connection
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-echo "Connected successfully";
+
+// Run the create table query
+if (mysqli_query($conn, '
+CREATE TABLE Prod (
+`Id` INT NOT NULL AUTO_INCREMENT ,
+`ProductName` VARCHAR(200) NOT NULL ,
+`Color` VARCHAR(50) NOT NULL ,
+`Price` DOUBLE NOT NULL ,
+PRIMARY KEY (`Id`)
+);
+')) {
+printf("Table created\n");
+}
+
+//Close the connection
+mysqli_close($conn);
 ?>
