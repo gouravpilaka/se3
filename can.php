29471364310daf76c39c51d28483859b061a5b0b
Gourav <60485831+gouravpilaka@users.noreply.github.com>
2020-05-15 22:35:41 -0400

    Update can.php

--- can.php
+++ can.php
@@ -8,10 +8,9 @@ function get_price($name)
 	$db   = "pilakag1_db";
     	$conn = new PDO("sqlsrv:Server = $host; Database = $db", $user, $pwd);
     	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$sql = ("select * from product where price=67;");
+	$sql = ("SELECT price FROM product WHERE product = '$name'");
 	$stmt = $conn->query("$sql");
 	$row = $stmt->fetch();
-	echo "$row[0] $row[1] $row[2]";
 	$price = $row["price"];
 	$conn = NULL;
  	return $price;	
