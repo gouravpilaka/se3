4f2dfc7cedc0ea594c4d75906d769f031ed5aed5
Gourav <60485831+gouravpilaka@users.noreply.github.com>
2020-05-15 21:28:54 -0400

    Update data.php

--- data.php
+++ data.php
@@ -1,8 +1,8 @@
 <?php
 
-
-function get_price($name)
+	if(!empty($_GET['name']))
 {
+	$name=$_GET['name'];
 	try {
 	$host = "tcp:localhost01.database.windows.net,1433";
 	$user = "pilakag1@localhost01";
@@ -11,26 +11,16 @@ function get_price($name)
     	$conn = new PDO("sqlsrv:Server = $host; Database = $db", $user, $pwd);
     	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$sql = ("SELECT price FROM product WHERE product = '$name'");
-	$result = $conn->query("$sql");
-	$row = $result->fetch();
-		
-	 if ($result->num_rows > 0) {
-             
-             while($row = $result->fetch_assoc()) {
-                      $price = $row["price"];
-            }
-           } 
-else {
-                     $price = null;
-        }
-
-    $conn = NULL;
-
-    return $price;
-	}
-     }	
+	$stmt = $conn->query("$sql");
+	$row = $stmt->fetch();
+	echo "$row[0] $row[1] $row[2]";
+	$conn = NULL;
+ 		}	
 	catch (Exception $e) {
     	die(print_r($e));
 	}
+}
 
-?>
+	
+		?>
+	 
