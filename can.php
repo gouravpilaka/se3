7505804924a06d29964c246eb0a95cd5f4f5e759
Gourav <60485831+gouravpilaka@users.noreply.github.com>
2020-05-15 20:45:13 -0400

    Update can.php

--- can.php
+++ can.php
@@ -9,16 +9,22 @@
 		<?php
 
 	try {
-    	$conn = new PDO("sqlsrv:Server = tcp:localhost01.database.windows.net,1433; Database = pilakag1_db", "pilakag1", "Pilakag_1");
+	$host = "tcp:localhost01.database.windows.net,1433";
+	$user = "pilakag1@localhost01";
+	$pwd  = "Pilakag_1";
+	$db   = "pilakag1_db";
+    	$conn = new PDO("sqlsrv:Server = $host; Database = $db", $user, $pwd);
     	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	
-
-	}
+	$sql = ("select * from product where price=67;");
+	$stmt = $conn->query("$sql");
+	$row = $stmt->fetch();
+	echo "$row[0] $row[1] $row[2]";
+	$conn = NULL;
+ 		}	
 	catch (Exception $e) {
-    	print("Error connecting to SQL Server.");
     	die(print_r($e));
 	}
-?>
-	</h1>
-</body>
+		?>
+	   </h1>
+	</body>
 </html>
